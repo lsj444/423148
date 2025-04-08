28ee2848abadc54220105efcfe5b1f4cf8e307cb
Leo <leosujith05@gmail.com>
2025-04-08 13:26:17 +0530

    Add files via upload

---
+++ 3.sh
@@ -0,0 +1,30 @@
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ chmod +x 301.sh
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ ./301.sh
+Enter a number:
+4
+4 is Even
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ chmod +x 302.sh
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ ./302.sh
+Enter a year:
+2024
+2024 is a Leap Year
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ chmod +x 303.sh
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ ./303.sh
+Enter a number:
+5
+Factorial of 5 is 120
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ chmod +x 304.sh
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ ./304.sh
+Enter first number:
+10
+Enter second number:
+20
+Before Swapping: a=10, b=20
+After Swapping: a=20, b=10
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ chmod +x 305.sh
+student@ai-HP-ProDesk-600-G4-MT:~/423148/423148$ ./305.sh
+Enter two numbers:
+15
+20
+GCD of 15 and 20 is: 5
+LCM of 15 and 20 is: 60
