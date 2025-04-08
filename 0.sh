53ff8ee6419e61e6db037b2eca3cb9edaaedf6ff
Leo <leosujith05@gmail.com>
2025-04-08 12:18:11 +0530

    Add files via upload

---
+++ 0.sh
@@ -0,0 +1,27 @@
+mkdir UNIX
+student@ai-HP-ProDesk-600-G4-MT:~$ cd UNIX
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX$ mkdir leo
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX$ cd leo
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ cal
+     April 2025       
+Su Mo Tu We Th Fr Sa  
+       1  2  3  4  5  
+ 6  7  8  9 10 11 12  
+13 14 15 16 17 18 19  
+20 21 22 23 24 25 26  
+27 28 29 30           
+                      
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ who
+student  :0           2025-04-08 11:36 (:0)
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ banner ART
+   #    ######  #######
+  # #   #     #    #
+ #   #  #     #    #
+#     # ######     #
+####### #   #      #
+#     # #    #     #
+#     # #     #    #
+
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ tty
+/dev/pts/0
+
