b63375bb4a385f3df3531243f46f51d541eb4482
Leo <leosujith05@gmail.com>
2025-04-08 12:24:28 +0530

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,17 @@
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ cal january 2025
+    January 2025      
+Su Mo Tu We Th Fr Sa  
+          1  2  3  4  
+ 5  6  7  8  9 10 11  
+12 13 14 15 16 17 18  
+19 20 21 22 23 24 25  
+26 27 28 29 30 31     
+                      
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ date
+Tuesday 08 April 2025 12:19:16 PM IST
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ uname
+Linux
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ logname
+student
+student@ai-HP-ProDesk-600-G4-MT:~/UNIX/leo$ hostname
+ai-HP-ProDesk-600-G4-MT
